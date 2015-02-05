b7e566172189fe2929f2396dc9b45ff13aa07eec
Robert Varga <rovarga@cisco.com>
2015-02-04 20:59:00 -0800

    Update benchmark
    Transaction chaining for binding-aware format
    PingPing buffer added - configurations, config yang model, initialization
    Modified the python test script
    Added error loggin to failed transaction path
    Added closing transaction chain
    
    Change-Id: I2cafa92ac75c6efbd0f5b78dbb1b3f9abfdeb0da
    Signed-off-by: Jan Medved <jmedved@cisco.com>
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- dsbenchmark/dsbenchmark-impl/src/main/java/org/opendaylight/dsbenchmark/DatastoreBaAbstractWrite.java
+++ dsbenchmark/dsbenchmark-impl/src/main/java/org/opendaylight/dsbenchmark/DatastoreBaAbstractWrite.java
@@ -2,10 +2,16 @@ package org.opendaylight.dsbenchmark;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChain;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchmark.rev150105.StartTestInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchmark.rev150105.TestExec;
@@ -18,60 +24,86 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchm
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 
-public abstract class DatastoreBaAbstractWrite implements DatastoreWrite {
+public abstract class DatastoreBaAbstractWrite implements DatastoreWrite, TransactionChainListener {
     private static final Logger LOG = LoggerFactory.getLogger(DatastoreBaAbstractWrite.class);
-    protected DataBroker dataBroker;
+    private final BindingTransactionChain chain;
+    private final long putsPerTx;
+    protected final DataBroker dataBroker;
     protected List<OuterList> list;
-    private long putsPerTx;
     protected int txOk = 0;
     protected int txError = 0;
+
+    // Shared instance for reuse
+    private final FutureCallback<Void> txCallback = new FutureCallback<Void>() {
+        @Override
+        public void onSuccess(final Void result) {
+            txOk++;
+        }
+        @Override
+        public void onFailure(final Throwable t) {
+            LOG.error("Transaction error: {}", t);
+            txError++;
+        }
+    };
 
     abstract protected void txOperation(WriteTransaction tx,
                                         LogicalDatastoreType dst, 
                                         InstanceIdentifier<OuterList> iid, 
                                         OuterList elem);
 
- public DatastoreBaAbstractWrite(StartTestInput input, DataBroker dataBroker) {
+    public DatastoreBaAbstractWrite(StartTestInput input, DataBroker dataBroker) {
         this.putsPerTx = input.getPutsPerTx();
         this.dataBroker = dataBroker;
+        this.chain = dataBroker.createTransactionChain(this);
     }
 
-     @Override
-     public void createList(StartTestInput input) {
+    @Override
+    public void createList(StartTestInput input) {
          list = buildOuterList(input.getOuterElements().intValue(), input.getInnerElements().intValue());
-     }
+    }
 
     @Override
     public void writeList() {
-        WriteTransaction tx = dataBroker.newWriteOnlyTransaction();
+        WriteTransaction tx = chain.newWriteOnlyTransaction();
+        CheckedFuture<Void, TransactionCommitFailedException> lastFuture = null;
         long putCnt = 0;
 
+        // Shared parent instance identifier
+        final InstanceIdentifier<TestExec> pid = InstanceIdentifier.create(TestExec.class);
         for (OuterList element : this.list) {
-            InstanceIdentifier<OuterList> iid = InstanceIdentifier.create(TestExec.class)
-                                                    .child(OuterList.class, element.getKey());
+            InstanceIdentifier<OuterList> iid = pid.child(OuterList.class, element.getKey());
             txOperation(tx, LogicalDatastoreType.CONFIGURATION, iid, element);
             putCnt++;
             if (putCnt == putsPerTx) {
-                try {
-                    tx.submit().checkedGet();
-                    txOk++;
-                } catch (TransactionCommitFailedException e) {
-                    LOG.error("Transaction failed: {}", e.toString());
-                    txError++;
-                }
-                tx = dataBroker.newWriteOnlyTransaction();
+                lastFuture = tx.submit();
+                Futures.addCallback(lastFuture, txCallback);
+                tx = chain.newWriteOnlyTransaction();
                 putCnt = 0;
             }
         }
 
         if (putCnt != 0) {
+            lastFuture = tx.submit();
+            Futures.addCallback(lastFuture, txCallback);
+        }
+
+        LOG.info("All transactions submitted, synchronizing on last transaction");
+        if (lastFuture != null) {
             try {
-                tx.submit().checkedGet();
-            } catch (TransactionCommitFailedException e) {
-                LOG.error("Transaction failed: {}", e.toString());
+                // This is not a complete guarantee all callback complete, but better
+                // than nothing. The issue is that a Future's value is set before
+                // callbacks are dispatched, so we still have a subtle race.
+                lastFuture.get(5, TimeUnit.SECONDS);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                LOG.error("Failed to complete last transaction within timeout", e);
             }
         }
+
+        LOG.info("Transaction completed, txCnt {}", (txOk + txError));
     }
 
     @Override
@@ -82,6 +114,19 @@ public abstract class DatastoreBaAbstractWrite implements DatastoreWrite {
     @Override
     public int getTxOk() {
         return txOk;
+    }
+
+    @Override
+    public void onTransactionChainFailed(TransactionChain<?, ?> chain,
+            AsyncTransaction<?, ?> transaction, Throwable cause) {
+        LOG.error("Broken chain in DatastoreBaAbstractWrite, transaction {}, cause {}",
+                transaction.getIdentifier(), cause);
+    }
+
+    @Override
+    public void onTransactionChainSuccessful(TransactionChain<?, ?> arg0) {
+        LOG.info("DatastoreBaAbstractWrite closed successfully");
+        
     }
 
     private List<OuterList> buildOuterList(int outerElements, int innerElements) {
