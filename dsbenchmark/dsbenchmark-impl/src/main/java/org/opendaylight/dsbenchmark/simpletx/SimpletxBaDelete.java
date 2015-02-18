cd6cfbe6380b9b53e8f040d42783bf140e653bc9
Jan Medved <jmedved@cisco.com>
2015-02-17 22:03:11 -0800

    Restructuring to prepare for addition of transaction chain samples/benchmark
    Change-Id: If33e8b1ee4de7b793a322c14274203bc53ac73c4
    Signed-off-by: Jan Medved <jmedved@cisco.com>

--- dsbenchmark/dsbenchmark-impl/src/main/java/org/opendaylight/dsbenchmark/simpletx/SimpletxBaDelete.java
+++ dsbenchmark/dsbenchmark-impl/src/main/java/org/opendaylight/dsbenchmark/simpletx/SimpletxBaDelete.java
@@ -1,9 +1,10 @@
-package org.opendaylight.dsbenchmark;
+package org.opendaylight.dsbenchmark.simpletx;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.dsbenchmark.DatastoreWrite;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchmark.rev150105.StartTestInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchmark.rev150105.TestExec;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchmark.rev150105.test.exec.OuterList;
@@ -12,15 +13,15 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DatastoreBaDelete implements DatastoreWrite {
-    private static final Logger LOG = LoggerFactory.getLogger(DatastoreBaDelete.class);
+public class SimpletxBaDelete implements DatastoreWrite {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpletxBaDelete.class);
     private DataBroker dataBroker;
     private long putsPerTx;
     private long outerElements;
     private int txOk = 0;
     private int txError = 0;
 
-    public DatastoreBaDelete(StartTestInput input, DataBroker dataBroker) {
+    public SimpletxBaDelete(StartTestInput input, DataBroker dataBroker) {
         LOG.info("Creating DatastoreDelete, input: {}", input );
         this.putsPerTx = input.getPutsPerTx();
         this.outerElements = input.getOuterElements();
@@ -71,7 +72,7 @@ public class DatastoreBaDelete implements DatastoreWrite {
     @Override
     public void createList(StartTestInput input) {
         LOG.info("DatastoreDelete: creating data in the data store");
-        DatastoreBaDump dd = new DatastoreBaDump(input, dataBroker);
+        SimpletxBaDump dd = new SimpletxBaDump(input, dataBroker);
         dd.createList(input);
         dd.writeList();
     }
--- dsbenchmark/dsbenchmark-impl/src/main/java/org/opendaylight/dsbenchmark/simpletx/SimpletxDomAbstractWrite.java
+++ dsbenchmark/dsbenchmark-impl/src/main/java/org/opendaylight/dsbenchmark/simpletx/SimpletxDomAbstractWrite.java
@@ -1,12 +1,13 @@
-package org.opendaylight.dsbenchmark;
+package org.opendaylight.dsbenchmark.simpletx;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
+import org.opendaylight.dsbenchmark.DatastoreWrite;
+import org.opendaylight.dsbenchmark.DomListBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchmark.rev150105.StartTestInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchmark.rev150105.TestExec;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dsbenchmark.rev150105.test.exec.OuterList;
@@ -15,14 +16,11 @@ import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
-import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
-import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
-import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.CollectionNodeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class DatastoreDomAbstractWrite implements DatastoreWrite {
-    private static final Logger LOG = LoggerFactory.getLogger(DatastoreDomAbstractWrite.class);
+public abstract class SimpletxDomAbstractWrite implements DatastoreWrite {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpletxDomAbstractWrite.class);
 
     // Inner List Qname identifiers for yang model's 'name' and 'value'
     private static final org.opendaylight.yangtools.yang.common.QName IL_NAME = QName.create(InnerList.QNAME, "name");
@@ -44,14 +42,16 @@ public abstract class DatastoreDomAbstractWrite implements DatastoreWrite {
                                         YangInstanceIdentifier yid, 
                                         MapEntryNode elem);
 
-    public DatastoreDomAbstractWrite(StartTestInput input, DOMDataBroker domDataBroker) {
+    public SimpletxDomAbstractWrite(StartTestInput input, DOMDataBroker domDataBroker) {
         this.domDataBroker = domDataBroker;
         this.putsPerTx = input.getPutsPerTx();
     }
 
     @Override
     public void createList(StartTestInput input) {
-        list = buildOuterList(input.getOuterElements().intValue(), input.getInnerElements().intValue());
+        list = DomListBuilder.buildOuterList(OL_ID, IL_NAME, IL_VALUE,
+                                             input.getOuterElements().intValue(),
+                                             input.getInnerElements().intValue());
     }
 
     @Override
@@ -95,33 +95,6 @@ public abstract class DatastoreDomAbstractWrite implements DatastoreWrite {
     @Override
     public int getTxOk() {
         return txOk;
-    }
-
-    private List<MapEntryNode> buildOuterList(int outerElements, int innerElements) {
-        List<MapEntryNode> outerList = new ArrayList<MapEntryNode>(outerElements);
-        for (int j = 0; j < outerElements; j++) {
-            outerList.add(ImmutableNodes.mapEntryBuilder()
-                                .withNodeIdentifier(new NodeIdentifierWithPredicates(OuterList.QNAME, OL_ID, j))
-                                .withChild(ImmutableNodes.leafNode(OL_ID, j))
-                                .withChild(buildInnerList(j, innerElements)) 
-                                .build());
-        }
-
-        return outerList;
-    }
-
-    private MapNode buildInnerList( int index, int elements ) {
-        CollectionNodeBuilder<MapEntryNode, MapNode> innerList = ImmutableNodes.mapNodeBuilder(InnerList.QNAME);
-
-        final String itemStr = "Item-" + String.valueOf(index) + "-";
-        for( int i = 0; i < elements; i++ ) {
-            innerList.addChild(ImmutableNodes.mapEntryBuilder()
-                                .withNodeIdentifier(new NodeIdentifierWithPredicates(InnerList.QNAME, IL_NAME, i))
-                                .withChild(ImmutableNodes.leafNode(IL_NAME, i))
-                                .withChild(ImmutableNodes.leafNode(IL_VALUE, itemStr + String.valueOf(i)))
-                                .build());
-        }
-        return innerList.build();
     }
 
 }
