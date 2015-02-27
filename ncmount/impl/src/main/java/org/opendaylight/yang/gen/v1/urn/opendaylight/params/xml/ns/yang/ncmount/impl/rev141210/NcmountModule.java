057eb23d7e08f1731c374a171325ab1cee02702b
Jan Medved <jmedved@cisco.com>
2015-02-27 14:55:08 -0800

    Moving the ncmount project onto the updated archetype
    Change-Id: I7ac12c9afd73c2e10a821b5b8d61d93441fcff2a
    Signed-off-by: Jan Medved <jmedved@cisco.com>

---
+++ ncmount/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/ncmount/impl/rev141210/NcmountModule.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 Cisco Systems and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.impl.rev141210;
+
+import ncmount.impl.NcmountProvider;
+
+public class NcmountModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.impl.rev141210.AbstractNcmountModule {
+    public NcmountModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public NcmountModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ncmount.impl.rev141210.NcmountModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        NcmountProvider provider = new NcmountProvider();
+        getBrokerDependency().registerProvider(provider);
+        return provider;
+    }
+
+}
