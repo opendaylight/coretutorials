c5fc6992298777caee7f5b99d9ebb35e761d8c45
Marian Adamjak <madamjak@cisco.com>
2015-04-07 10:10:14 +0200

    Hello world event source sample app
       - modify create of notification
       - add simple user agent for testing purposes
    
    Change-Id: If7bc8c44183970aa92932263dd0410afd46cdc68
    Signed-off-by: Marian Adamjak <madamjak@cisco.com>

---
+++ hweventsource/uagent/src/main/java/org/opendaylight/controller/hweventsource/uagent/NoopBindingConsumer.java
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2015 Cisco Systems, Inc. and others. All rights reserved. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.hweventsource.uagent;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ConsumerContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoopBindingConsumer implements BindingAwareConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NoopBindingConsumer.class);
+
+    @Override
+    public void onSessionInitialized(ConsumerContext session) {
+        LOG.info("NoopBindingConsumer initialized");
+    }
+
+}
