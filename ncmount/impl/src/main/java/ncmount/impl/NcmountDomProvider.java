a8602d68f79295857b37772f7cc3a2918fd58688
subhash <subh.singh007@gmail.com>
2015-07-02 00:39:47 +0530

    0a977f662d90a24048024c6f5287232396d2d411: 1. fixed the typo in the config file. 2. Added the empty implemetation for the RPC. 3. Implemented show-node for the config nodes.
    Change-Id: Ie144b123622d259c03890d4d102a294be6e70553
    Signed-off-by: subhash <subh.singh007@gmail.com>

--- ncmount/impl/src/main/java/ncmount/impl/NcmountDomProvider.java
+++ ncmount/impl/src/main/java/ncmount/impl/NcmountDomProvider.java
@@ -1,14 +1,17 @@
 package ncmount.impl;
 
-import com.google.common.util.concurrent.CheckedFuture;
 import java.util.Collection;
 import java.util.Collections;
+
 import javax.annotation.Nonnull;
+
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataChangeListener;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.dom.api.DOMMountPointService;
 import org.opendaylight.controller.md.sal.dom.api.DOMNotificationService;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcException;
@@ -17,6 +20,7 @@ import org.opendaylight.controller.md.sal.dom.api.DOMRpcImplementation;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcProviderService;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcResult;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
+import org.opendaylight.controller.md.sal.dom.spi.DefaultDOMRpcResult;
 import org.opendaylight.controller.sal.core.api.Broker;
 import org.opendaylight.controller.sal.core.api.Provider;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.network.topology.topology.topology.types.TopologyNetconf;
@@ -26,40 +30,62 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.ListNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
 import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.Futures;
 
 public class NcmountDomProvider implements Provider, AutoCloseable, DOMRpcImplementation, DOMDataChangeListener {
 
+    private static final Logger LOG = LoggerFactory.getLogger(NcmountDomProvider.class);
+
     public static final YangInstanceIdentifier NETCONF_TOPO_IID;
-    private static final DOMRpcIdentifier SHOW_NODE_RPC_ID = DOMRpcIdentifier.create(SchemaPath.create(true, QName.create(ShowNodeInput.QNAME, "show-node")));
+    private static final DOMRpcIdentifier SHOW_NODE_RPC_ID = DOMRpcIdentifier.create(SchemaPath.create(true, QName.create(ShowNodeInput.QNAME, "show-node"))); 
+    private static final DOMRpcIdentifier LIST_NODES_ID = DOMRpcIdentifier.create(SchemaPath.create(true, QName.create("urn:opendaylight:params:xml:ns:yang:ncmount", "2015-01-05", "list-nodes")));
+    private static final DOMRpcIdentifier WRITE_NODES_ID = DOMRpcIdentifier.create(SchemaPath.create(true, QName.create("urn:opendaylight:params:xml:ns:yang:ncmount", "2015-01-05", "write-routes")));
 
     static {
         final org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.InstanceIdentifierBuilder builder =
                 org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.builder();
         builder
-                .node(NetworkTopology.QNAME)
-                .node(Topology.QNAME)
-                .nodeWithKey(Topology.QNAME, QName.create(Topology.QNAME, "topology-id"), TopologyNetconf.QNAME.getLocalName());
+        .node(NetworkTopology.QNAME)
+        .node(Topology.QNAME)
+        .nodeWithKey(Topology.QNAME, QName.create(Topology.QNAME, "topology-id"), TopologyNetconf.QNAME.getLocalName());
 
         NETCONF_TOPO_IID = builder.build();
     }
 
     private DOMMountPointService mountPointService;
+    private DOMDataBroker globalDomDataBroker;
 
     @Override
     public void onSessionInitiated(final Broker.ProviderSession providerSession) {
         // get the DOM versions of MD-SAL services
-        final DOMDataBroker dataBroker = providerSession.getService(DOMDataBroker.class);
+        this.globalDomDataBroker = providerSession.getService(DOMDataBroker.class);
         this.mountPointService = providerSession.getService(DOMMountPointService.class);
 
         final DOMRpcProviderService service = providerSession.getService(DOMRpcProviderService.class);
-        service.registerRpcImplementation(this, SHOW_NODE_RPC_ID);
-        // TODO do the same for 2 other rpcs
+        service.registerRpcImplementation(this, SHOW_NODE_RPC_ID, LIST_NODES_ID, WRITE_NODES_ID);
 
         final YangInstanceIdentifier nodeIid = YangInstanceIdentifier.builder(NETCONF_TOPO_IID).node(Node.QNAME).build();
 
-        dataBroker.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL,
+        LOG.info("NcmountDomProvider is registered");
+
+        this.globalDomDataBroker.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL,
                 nodeIid,
                 this,
                 AsyncDataBroker.DataChangeScope.SUBTREE);
@@ -75,14 +101,28 @@ public class NcmountDomProvider implements Provider, AutoCloseable, DOMRpcImplem
     @Override
     public void close() throws Exception {
         // TODO close resources
+        this.globalDomDataBroker = null;
+        this.mountPointService = null;
     }
 
     @Nonnull
     @Override
     public CheckedFuture<DOMRpcResult, DOMRpcException> invokeRpc(@Nonnull final DOMRpcIdentifier domRpcIdentifier, final NormalizedNode<?, ?> normalizedNode) {
+
+        // I think, we can replace following with the switch
+        // statement http://stackoverflow.com/questions/10240538/use-string-in-switch-case-in-java
         if(domRpcIdentifier.equals(SHOW_NODE_RPC_ID)) {
             return showNode(normalizedNode);
         }
+
+        if (domRpcIdentifier.equals(LIST_NODES_ID)) {
+            return listNodes();
+        }
+
+        if (domRpcIdentifier.equals(WRITE_NODES_ID)) {
+            return writeNode(normalizedNode);
+        }
+
         return null;
     }
 
@@ -92,10 +132,69 @@ public class NcmountDomProvider implements Provider, AutoCloseable, DOMRpcImplem
         // Normalized node is the generic supertype for all normalized nodes e.g. container node, map node etc.
         // Its really important to know the normalized node structure when trying to parse/itarate over normalized nodes
 
-        final YangInstanceIdentifier iid = null;
+        YangInstanceIdentifier iid = null;
+
         mountPointService.getMountPoint(iid).get().getService(DOMDataBroker.class).get().newReadOnlyTransaction();
         mountPointService.getMountPoint(iid).get().getService(DOMRpcService.class).get().invokeRpc(null, null);
         mountPointService.getMountPoint(iid).get().getService(DOMNotificationService.class);
+        return null;
+    }
+
+    private CheckedFuture<DOMRpcResult, DOMRpcException> listNodes() {
+        LOG.info(" invoked RPC List-Node");
+
+        QName RPC_OUTPUT_QNAME = QName.create("urn:opendaylight:params:xml:ns:yang:ncmount", "2015-01-05", "output");
+        QName NC_CONFIG_NODES = QName.create("urn:opendaylight:params:xml:ns:yang:ncmount", "2015-01-05", "nc-config-nodes");
+        DOMDataReadOnlyTransaction rtx = this.globalDomDataBroker.newReadOnlyTransaction();
+        NormalizedNode<?, ?> ncNodes = null;
+
+        // read the config datastore for the available nodes.
+        try {
+            ncNodes = rtx.read(LogicalDatastoreType.CONFIGURATION, NETCONF_TOPO_IID).checkedGet().get();
+        } catch (ReadFailedException e) {
+            LOG.warn(e.toString());
+            return Futures.immediateFailedCheckedFuture((DOMRpcException) 
+                    new DOMRpcException("rpc invocation not implemented yet") {
+            });
+        }
+
+        // created the leaf node to represent "leaf-list nc-config-nodes"
+        ListNodeBuilder<Object, LeafSetEntryNode<Object>> leafListBuilder = Builders.leafSetBuilder()
+                .withNodeIdentifier(new YangInstanceIdentifier.NodeIdentifier(NC_CONFIG_NODES));
+
+        if (ncNodes instanceof MapEntryNode) {
+
+            // get all the nodes from the ncNode
+            NodeIdentifier nodeid = new NodeIdentifier(Node.QNAME);
+            DataContainerChild<? extends PathArgument, ?> data = ((MapEntryNode) ncNodes).getChild(nodeid).get();
+
+            if (data instanceof MapNode) {
+                for (MapEntryNode node : ((MapNode) data).getValue()) {
+
+                    for(DataContainerChild<? extends PathArgument, ?> nodeChild : node.getValue()) {
+                        if (nodeChild instanceof LeafNode) {
+                            LOG.info("leaf node info : {}", nodeChild.getValue());
+                            // add the node value to the leaf-list.
+                            leafListBuilder.withChildValue(nodeChild.getValue());
+                        }
+                    }
+                }
+            }
+        }
+
+        // pack the output and return.
+        ContainerNode resultNode = ImmutableContainerNodeBuilder.create()
+                .withNodeIdentifier(new YangInstanceIdentifier.NodeIdentifier(RPC_OUTPUT_QNAME))
+                .withChild(leafListBuilder.build())
+                .build();
+        LOG.info("return the value : {}", resultNode);
+
+        return Futures.immediateCheckedFuture((DOMRpcResult) new DefaultDOMRpcResult(resultNode));
+    }
+
+    private CheckedFuture<DOMRpcResult, DOMRpcException> writeNode(final NormalizedNode<?, ?> normalizedNode) {
+        // TODO: write the implementation for the Write-Node.
+        LOG.info("invoked RPC Write-Node");
         return null;
     }
 
