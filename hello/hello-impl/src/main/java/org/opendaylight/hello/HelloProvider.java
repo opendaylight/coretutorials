598d98a2546c9c30762779537d5512ec99b9c694
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-08 00:53:51 -0400

    Fix license header violations in hello
    Change-Id: I48915f180665641c7230f50826d94dbba431529e
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- hello/hello-impl/src/main/java/org/opendaylight/hello/HelloProvider.java
+++ hello/hello-impl/src/main/java/org/opendaylight/hello/HelloProvider.java
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) Yoyodyne, Inc. and others.  All rights reserved.
+ * Copyright (c) 2015 Yoyodyne, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.hello;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
