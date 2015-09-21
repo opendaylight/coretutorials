8cb8a311c124abecc81b744100bb47ed94792442
Maros Marsalek <mmarsale@cisco.com>
2015-09-21 15:07:20 +0200

    Dhcp server
    Change-Id: Id1fca29155c7cc3ede45340ddb7a8dc92d64f25a
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    Signed-off-by: Kinsey Nietzsche <kinsey.nietzsche@pantheon.sk>

---
+++ dhcp/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/dhcp/impl/v6/rev151018/ExampleV6leaseManagerModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.impl.v6.rev151018;
+public class ExampleV6leaseManagerModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.impl.v6.rev151018.AbstractExampleV6leaseManagerModule {
+    public ExampleV6leaseManagerModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public ExampleV6leaseManagerModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, ExampleV6leaseManagerModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
