671bcab46813d782e23040126f71679aff841050
Maros Marsalek <mmarsale@cisco.com>
2015-09-21 15:07:20 +0200

    Dhcp server
    This sample application incorporates extensible DHCP v4 and v6 into ODL. There
    are example lease managers that provide some static IPs, but these serve only
    as examples and should not be used in real deployment.
    
    Change-Id: Id1fca29155c7cc3ede45340ddb7a8dc92d64f25a
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>
    Signed-off-by: Kinsey Nietzsche <kinsey.nietzsche@pantheon.sk>

---
+++ dhcp/impl/src/main/java/org/opendaylight/dhcp/server/CustomisableLeaseManagerDhcpService.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2015 Cisco Systems and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.dhcp.server;
+
+import java.util.List;
+
+import org.anarres.dhcp.v6.options.Dhcp6Option;
+import org.apache.directory.server.dhcp.DhcpException;
+import org.apache.directory.server.dhcp.io.DhcpRequestContext;
+import org.apache.directory.server.dhcp.messages.DhcpMessage;
+import org.apache.directory.server.dhcp.options.DhcpOption;
+import org.apache.directory.server.dhcp.options.OptionsField;
+import org.apache.directory.server.dhcp.service.manager.LeaseManager;
+import org.apache.directory.server.dhcp.service.manager.LeaseManagerDhcpService;
+import org.opendaylight.dhcpv6.server.CustomDhcpv6Option;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.impl.rev151018.DefaultOption;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.impl.rev151018.MessageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple extension to LeaseManagerDhcpService that provides a set of predefined options
+ */
+public class CustomisableLeaseManagerDhcpService extends LeaseManagerDhcpService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CustomisableLeaseManagerDhcpService.class);
+
+    private final OptionsField dhcpOfferOptionField;
+    private final OptionsField dhcpAckOptionField;
+    private final OptionsField dhcpNakOptionField;
+
+    public CustomisableLeaseManagerDhcpService(LeaseManager manager, List<DefaultOption> options)
+            throws IllegalArgumentException {
+        super(manager);
+        dhcpOfferOptionField = new OptionsField();
+        dhcpAckOptionField = new OptionsField();
+        dhcpNakOptionField = new OptionsField();
+        implementOptions(options);
+    }
+
+    @Override
+    protected DhcpMessage handleDISCOVER(DhcpRequestContext context, DhcpMessage request) throws DhcpException {
+        LOG.trace("Creating DHCP DISCOVER message");
+        DhcpMessage message = super.handleDISCOVER(context, request);
+        if (message != null) {
+            message.getOptions().addAll(dhcpOfferOptionField);
+        }
+        return message;
+    }
+
+    @Override
+    protected DhcpMessage handleREQUEST(DhcpRequestContext context, DhcpMessage request) throws DhcpException {
+        LOG.trace("Creating DHCP REQUEST message");
+        DhcpMessage message = super.handleREQUEST(context, request);
+        if (message != null) {
+            message.getOptions().addAll(dhcpAckOptionField);
+        }
+        return message;
+    }
+
+    @Override
+    protected DhcpMessage handleDECLINE(DhcpRequestContext context, DhcpMessage request) throws DhcpException {
+        LOG.trace("Creating DHCP DECLINE message");
+        DhcpMessage message = super.handleDECLINE(context, request);
+        if (message != null) {
+            message.getOptions().addAll(dhcpNakOptionField);
+        }
+        return message;
+    }
+
+    @Override
+    protected DhcpMessage handleRELEASE(DhcpRequestContext context, DhcpMessage request) throws DhcpException {
+        LOG.trace("Creating DHCP RELEASE message");
+        return super.handleRELEASE(context, request);
+    }
+
+    protected void implementOptions(List<DefaultOption> options) {
+        DhcpOption dhcpOption;
+        MessageType scope;
+        for (DefaultOption o : options) {
+            try {
+                dhcpOption = new CustomDhcpOption((byte) o.getId().intValue(), o.getValue());
+            } catch (IllegalArgumentException e) {
+                LOG.warn("Failed to parse DHCP option {}, skipping implementation", o.getId());
+                continue;
+            }
+            scope = o.getScope();
+            if (scope == MessageType.DHCPOFFER || scope == MessageType.ALL) {
+                dhcpOfferOptionField.add(dhcpOption);
+                LOG.debug("DHCP OFFER option {} implemented", o.getId());
+            }
+            if (scope == MessageType.DHCPACK || scope == MessageType.ALL) {
+                dhcpAckOptionField.add(dhcpOption);
+                LOG.debug("DHCP ACK option {} implemented", o.getId());
+            }
+            if (scope == MessageType.DHCPNAK || scope == MessageType.ALL) {
+                dhcpNakOptionField.add(dhcpOption);
+                LOG.debug("DHCP NAK option {} implemented", o.getId());
+            }
+        }
+    }
+
+}
---
+++ dhcp/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/dhcp/impl/v6/rev151018/Dhcpv6Module.java
@@ -0,0 +1,88 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.impl.v6.rev151018;
+
+import java.net.SocketException;
+import java.util.HashSet;
+import java.util.List;
+
+import org.anarres.dhcp.v6.service.Dhcp6LeaseManager;
+import org.anarres.dhcp.v6.service.Dhcp6Service;
+import org.opendaylight.controller.config.api.JmxAttributeValidationException;
+import org.opendaylight.dhcpv6.server.CustomisableLeaseManagerDhcpv6Service;
+import org.opendaylight.dhcpv6.server.Dhcpv6Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.primitives.Shorts;
+
+import io.netty.channel.EventLoopGroup;
+
+public class Dhcpv6Module extends
+        org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.impl.v6.rev151018.AbstractDhcpv6Module {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Dhcpv6Module.class);
+
+    public Dhcpv6Module(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public Dhcpv6Module(org.opendaylight.controller.config.api.ModuleIdentifier identifier,
+            org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, Dhcpv6Module oldModule,
+            java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        JmxAttributeValidationException.checkNotNull(getPort(), "port-number not set", portJmxAttribute);
+        Integer portNumber = getPort().getValue();
+        JmxAttributeValidationException.checkCondition((portNumber > 0 && portNumber < 65535),
+                "port-number out of range", portJmxAttribute);
+        JmxAttributeValidationException.checkNotNull(getNetworkInterface(), "network-interface not set",
+                networkInterfaceJmxAttribute);
+        JmxAttributeValidationException.checkNotNull(getServerId(), "server-id", serverIdJmxAttribute);
+        JmxAttributeValidationException.checkNotNull(getLeaseManager(), "lease-manager not set",
+                leaseManagerJmxAttribute);
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        LOG.debug("Starting DHCPv6 server on port {}", getPort());
+
+        return startServer(getPort().getValue().shortValue(), getNetworkInterface(), getLeaseManagerDependency(),
+                getDefaultOption(), getWorkerThreadGroupDependency(), getServerId().shortValue());
+    }
+
+    private static AutoCloseable startServer(final Short port, final List<String> networkInterfaces,
+            final Dhcp6LeaseManager manager, final List<DefaultOption> options, EventLoopGroup eventLoopGroup,
+            Short serverDuid) {
+        final Dhcp6Service dhcp6Service =
+                new CustomisableLeaseManagerDhcpv6Service(manager, Shorts.toByteArray(serverDuid), options);
+
+        final Dhcpv6Server dhcpv6Server;
+        try {
+            dhcpv6Server = new Dhcpv6Server(dhcp6Service, serverDuid, port, new HashSet<String>(networkInterfaces));
+        } catch (SocketException e) {
+            LOG.error("DHCPv6 server on port {} failed to initialise {}", e.toString());
+            throw new IllegalStateException(e);
+        }
+
+        try {
+            dhcpv6Server.start(eventLoopGroup);
+            LOG.info("DHCPv6 server on port {} started", port);
+            return new AutoCloseable() {
+
+                @Override
+                public void close() throws Exception {
+                    LOG.debug("Stopping DHCPv6 server on port {}", port);
+                    dhcpv6Server.stop();
+                    LOG.info("DHCPv6 server on port {} stopped", port);
+                }
+            };
+        } catch (Exception e) {
+            LOG.warn("DHCPv6 server on port {} failed to start {}", e.toString());
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
