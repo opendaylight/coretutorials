8a89c2b25817130e3f621afc29984c04009f090a
Jan Medved <jmedved@cisco.com>
2015-12-15 20:27:30 -0800

    Added the benchmark app - an empty app that builds a controller with its built-in benchmarks installed
    Change-Id: Ia17fa6a9567f5c2941e5a344a1fd2ba671a5ccf0
    Signed-off-by: Jan Medved <jmedved@cisco.com>

---
+++ benchmark/it/src/test/java/benchmark/it/BenchmarkIT.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2015 Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package benchmark.it;
+
+import static org.ops4j.pax.exam.CoreOptions.composite;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opendaylight.controller.mdsal.it.base.AbstractMdsalTestBase;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
+import org.ops4j.pax.exam.options.MavenUrlReference;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class BenchmarkIT extends AbstractMdsalTestBase {
+    private static final Logger LOG = LoggerFactory.getLogger(BenchmarkIT.class);
+
+    @Override
+    public String getModuleName() {
+        return "benchmark";
+    }
+
+    @Override
+    public String getInstanceName() {
+        return "benchmark-default";
+    }
+
+    @Override
+    public MavenUrlReference getFeatureRepo() {
+        return maven()
+                .groupId("org.opendaylight.coretutorials")
+                .artifactId("benchmark-features")
+                .classifier("features")
+                .type("xml")
+                .versionAsInProject();
+    }
+
+    @Override
+    public String getFeatureName() {
+        return "odl-benchmark-ui";
+    }
+
+    @Override
+    public Option getLoggingOption() {
+        Option option = editConfigurationFilePut(ORG_OPS4J_PAX_LOGGING_CFG,
+                logConfiguration(BenchmarkIT.class),
+                LogLevel.INFO.name());
+        option = composite(option, super.getLoggingOption());
+        return option;
+    }
+
+    @Test
+    public void testbenchmarkFeatureLoad() {
+        Assert.assertTrue(true);
+    }
+}
