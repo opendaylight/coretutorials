e2d53ad31b621ad9677ef13acbdb8b7fd6b48945
Jan Medved <jmedved@cisco.com>
2016-08-08 10:56:48 -0700

    Basic *working* simple writes into 2 shards Added docker creation script Added ansible config script
    Change-Id: I45ec00af05009df076fef38e9a275aeb2b64fc02
    Signed-off-by: Jan Medved <jmedved@cisco.com>

--- clustering/shardingsimple/impl/src/main/java/sharding/simple/impl/ShardingSimpleProvider.java
+++ clustering/shardingsimple/impl/src/main/java/sharding/simple/impl/ShardingSimpleProvider.java
@@ -8,16 +8,23 @@
 
 package sharding.simple.impl;
 
+import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+
 import org.opendaylight.controller.sal.core.api.model.SchemaService;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
-import org.opendaylight.mdsal.dom.api.DOMDataBroker;
+import org.opendaylight.mdsal.common.api.TransactionCommitFailedException;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeCursorAwareTransaction;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeListener;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeListeningException;
+import org.opendaylight.mdsal.dom.api.DOMDataTreeLoopException;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeProducer;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeProducerException;
 import org.opendaylight.mdsal.dom.api.DOMDataTreeService;
@@ -32,13 +39,18 @@ import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.util.concurrent.SpecialExecutors;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 /**
  * @author jmedved
@@ -53,8 +65,6 @@ public class ShardingSimpleProvider implements SchemaContextListener {
     private static final int DATA_CHANGE_LISTENER_MAX_QUEUE_SIZE = 1000;
     private static final int COMMIT_MAX_QUEUE_SIZE = 1000;
 
-    private final DataBroker dataBroker;
-    private final DOMDataBroker domBroker;
     private final DOMDataTreeShardingService dataTreeShardingService;
     private final DOMDataTreeService dataTreeService;
     private final SchemaService schemaService;
@@ -65,17 +75,13 @@ public class ShardingSimpleProvider implements SchemaContextListener {
 
 
     /** Public constructor - references to MD-SAL services injected through here.
-     * @param dataBroker:
-     * @param dataTreeShardingService:
-     * @param dataTreeService:
+     * @param dataTreeShardingService
+     * @param dataTreeService
+     * @param schemaService
      */
-    public ShardingSimpleProvider(final DataBroker dataBroker,
-                                  final DOMDataBroker domBroker,
-                                  final DOMDataTreeShardingService dataTreeShardingService,
+    public ShardingSimpleProvider(final DOMDataTreeShardingService dataTreeShardingService,
                                   final DOMDataTreeService dataTreeService,
                                   final SchemaService schemaService) {
-        this.dataBroker = dataBroker;
-        this.domBroker = domBroker;
         this.dataTreeShardingService = dataTreeShardingService;
         this.dataTreeService = dataTreeService;
         this.schemaService = schemaService;
@@ -107,7 +113,7 @@ public class ShardingSimpleProvider implements SchemaContextListener {
             try {
                 producer.close();
             } catch (final DOMDataTreeProducerException e) {
-                LOG.error("Exception closing root producer: {}", e);
+                throw new RuntimeException(e);
             }
         } else {
             LOG.error("Could not register root shard");
@@ -125,6 +131,30 @@ public class ShardingSimpleProvider implements SchemaContextListener {
                                                                 DATA_CHANGE_LISTENER_MAX_QUEUE_SIZE,
                                                                 COMMIT_MAX_QUEUE_SIZE);
 
+        LOG.info("Creatinglist to write to Shard1");
+        final MapNode list1 = DomListBuilder.buildInnerList(1, 10);
+
+        LOG.info("Creating a transaction chain for the producer");
+        final DOMDataTreeCursorAwareTransaction tx1 = producerShard1.createTransaction(false);
+        final DOMDataTreeWriteCursor cursor1 = tx1.createCursor(dtiShard1);
+        final YangInstanceIdentifier list1Yid = shard1Yid.node(InnerList.QNAME);
+        cursor1.write(list1Yid.getLastPathArgument(), list1);
+        cursor1.enter(new NodeIdentifier(InnerList.QNAME));
+
+        for ( MapEntryNode item : list1.getValue()) {
+            // YangInstanceIdentifier itemYid = list1Yid.node(new NodeIdentifierWithPredicates(InnerList.QNAME,
+            //                                                item.getIdentifier().getKeyValues()));
+            cursor1.write(new NodeIdentifierWithPredicates(InnerList.QNAME, item.getIdentifier().getKeyValues()), item);
+        }
+        cursor1.close();
+
+        LOG.info("Submitting transaction tx1");
+        try {
+            tx1.submit().checkedGet();
+        } catch (TransactionCommitFailedException e) {
+            throw new RuntimeException(e);
+        }
+
         LOG.info("Registering shard at Shard2");
         final YangInstanceIdentifier shard2Yid = testDataYid.node(new NodeIdentifierWithPredicates(OuterList.QNAME,
                 QName.create(OuterList.QNAME, "oid"),
@@ -136,25 +166,28 @@ public class ShardingSimpleProvider implements SchemaContextListener {
                                                                 DCL_EXECUTOR_MAX_QUEUE_SIZE,
                                                                 DATA_CHANGE_LISTENER_MAX_QUEUE_SIZE,
                                                                 COMMIT_MAX_QUEUE_SIZE);
+        final DOMDataTreeCursorAwareTransaction tx2 = producerShard2.createTransaction(false);
+        final DOMDataTreeWriteCursor cursor2 = tx2.createCursor(dtiShard2);
 
-        LOG.info("Creatinglist to write to Shard1");
-        final MapNode list1 = DomListBuilder.buildInnerList(1, 10);
+        LOG.info("Creatinglist to write to Shard2");
+        final MapNode list2 = DomListBuilder.buildInnerList(2, 10);
+        cursor2.write(new NodeIdentifier(InnerList.QNAME), list2);
 
-        LOG.info("Creating a transaction chain for the producer");
-        DOMDataTreeCursorAwareTransaction tx = producerShard1.createTransaction(false);
-        DOMDataTreeWriteCursor cursor = tx.createCursor(dtiShard1);
-        YangInstanceIdentifier list1Yid = shard1Yid.node(InnerList.QNAME);
-        // cursor.write(list1Yid.getLastPathArgument(), list1);
+        cursor2.close();
 
-        /*
-        for ( MapEntryNode item : list1.getValue()) {
-            // YangInstanceIdentifier itemYid = list1Yid.node(new NodeIdentifierWithPredicates(InnerList.QNAME, item.getIdentifier().getKeyValues()));
-            cursor.write(new NodeIdentifierWithPredicates(InnerList.QNAME, item.getIdentifier().getKeyValues()), item);
+        LOG.info("Submitting transaction tx2");
+        try {
+            tx2.submit().checkedGet();
+        } catch (TransactionCommitFailedException e) {
+            throw new RuntimeException(e);
         }
-        cursor.close();
-        LOG.info("Submitting transaction");
-        tx.submit();
-        */
+
+        try {
+            dataTreeService.registerListener(new TestListener(),
+                    Lists.newArrayList(dtiShard1, dtiShard2), false, Collections.emptyList());
+        } catch (final DOMDataTreeLoopException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -215,4 +248,20 @@ public class ShardingSimpleProvider implements SchemaContextListener {
     public void onGlobalContextUpdated(final SchemaContext schemaContext) {
         shards.forEach(shard -> shard.onGlobalContextUpdated(schemaContext));
     }
+
+    public static final class TestListener implements DOMDataTreeListener {
+
+        @Override
+        public void onDataTreeChanged(final Collection<DataTreeCandidate> collection,
+                final Map<DOMDataTreeIdentifier, NormalizedNode<?, ?>> map) {
+            LOG.warn("Received onDataTreeChanged {}, data: {}", collection, map);
+        }
+
+        @Override
+        public void onDataTreeFailed(final Collection<DOMDataTreeListeningException> collection) {
+            LOG.error("Received onDataTreeFailed {}", collection);
+
+        }
+    }
+
 }
