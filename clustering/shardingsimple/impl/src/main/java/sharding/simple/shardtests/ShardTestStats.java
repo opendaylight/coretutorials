b362daa2af1bfd99d59c15e9fc6b4e4b0a80bda1
Jan <jmedved@cisco.com>
2016-08-14 00:59:07 -0700

    Refactoring sharding test, adding RPC service API - Moved the test execution into a dedicated class. Test execution   triggered by RPC API. - Refactoring packaging - added a package for all stuff related   to shard tests - Added test handlers and their stats collecting
    Change-Id: I4eb7e4b707702c0ac7810db374151aa8963f889c
    Signed-off-by: Jan <jmedved@cisco.com>

---
+++ clustering/shardingsimple/impl/src/main/java/sharding/simple/shardtests/ShardTestStats.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package sharding.simple.shardtests;
+
+/**
+ * @author jmedved
+ *
+ */
+public class ShardTestStats {
+    private final long txOk;
+    private final long txError;
+    private final long txSubmitted;
+    private final long execTime;
+    private final long listenerEventsOk;
+    private final long listenerEventsFail;
+
+    ShardTestStats(int txOk, int txError, int txSubmitted, long execTime,
+            long dataTreeEventsOk, long dataTreeEventsFail) {
+        this.txOk = txOk;
+        this.txError = txError;
+        this.txSubmitted = txSubmitted;
+        this.execTime = execTime;
+        this.listenerEventsOk = dataTreeEventsOk;
+        this.listenerEventsFail = dataTreeEventsFail;
+    }
+
+    public long getTxOk() {
+        return txOk;
+    }
+
+    public long getTxError() {
+        return txError;
+    }
+
+    public long getTxSubmitted() {
+        return txSubmitted;
+    }
+
+    public long getExecTime() {
+        return execTime;
+    }
+
+    public long getListenerEventsOk() {
+        return listenerEventsOk;
+    }
+
+    public long getListenerEventsFail() {
+        return listenerEventsFail;
+    }
+
+}
