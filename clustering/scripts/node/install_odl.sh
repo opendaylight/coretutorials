aca73aa63a8a88f7f1315df1b1403de691adb92b
Jan <jmedved@cisco.com>
2016-08-29 18:54:52 -0700

    - Added copyright headers to scritps - Modified the Dockerfile that builds ODL-ready containers - Added script user manual adoc
    Change-Id: I60217e7edab1f2d7e98dbe41e137f38dafc56136
    Signed-off-by: Jan <jmedved@cisco.com>

--- clustering/scripts/node/install_odl.sh
+++ clustering/scripts/node/install_odl.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-
+#!/bin/bash#
+#
+# Copyright (c) 2016 Cisco Systems, Inc. and others. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+#
 # author__ = "Jan Medved"
-# copyright__ = "Copyright(c) 2016, Cisco Systems, Inc."
-# license__ = "Eclipse Public License (EPL) v1.0"
-# email__ = "jmedved@cisco.com"
+#
 
 # Init our own program name
 program_name=$0
