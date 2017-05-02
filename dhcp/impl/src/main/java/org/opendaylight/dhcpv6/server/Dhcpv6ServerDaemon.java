61c89c74e1dc1a5d6c7fd99b5e980c9ba41f753b
Robert Varga <rovarga@cisco.com>
2017-05-02 14:17:31 +0200

    Fix dlux/dluxapps references
    We really want to pull in yangman, hence we need to update to
    it being split out.
    
    Change-Id: I87739f4ac26b905b7785e5250e8b750dd4bf5154
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- dhcp/impl/src/main/java/org/opendaylight/dhcpv6/server/Dhcpv6ServerDaemon.java
+++ dhcp/impl/src/main/java/org/opendaylight/dhcpv6/server/Dhcpv6ServerDaemon.java
@@ -13,15 +13,12 @@ import java.io.IOException;
 import java.net.SocketException;
 import java.util.HashSet;
 import java.util.List;
-
 import org.anarres.dhcp.v6.service.Dhcp6LeaseManager;
 import org.anarres.dhcp.v6.service.Dhcp6Service;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.rev161018.DhcpServerCfg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.rev161018.DhcpServerCfg;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.dhcp.rev161018.dhcp.server.cfg.DefaultOption;
 
 /**
  * Dhcpv6Server Deamon,as blueprint bean.
@@ -34,14 +31,16 @@ public class Dhcpv6ServerDaemon {
     private final DataBroker dataBroker;
 
     public Dhcpv6ServerDaemon(final DataBroker dataBroker,
-                              final EventLoopGroup eventLoopGroup, final Dhcp6LeaseManager manager, short serverId, final DhcpServerCfg dhcpServerCfg) {
+                              final EventLoopGroup eventLoopGroup, final Dhcp6LeaseManager manager,
+                              final short serverId, final DhcpServerCfg dhcpServerCfg) {
         this.dataBroker = dataBroker;
         this.eventLoopGroup = eventLoopGroup;
         final int port = dhcpServerCfg.getPort().getValue();
         final List<String> networkInterfaces = dhcpServerCfg.getNetworkInterface();
-        dhcpService = new CustomisableLeaseManagerDhcpv6Service(manager, Shorts.toByteArray(serverId),dhcpServerCfg.getDefaultOption());
+        dhcpService = new CustomisableLeaseManagerDhcpv6Service(manager, Shorts.toByteArray(serverId),
+            dhcpServerCfg.getDefaultOption());
         try {
-            dhcpServer = new Dhcpv6Server(dhcpService,serverId, (short)port,new HashSet<String>(networkInterfaces));
+            dhcpServer = new Dhcpv6Server(dhcpService,serverId, (short)port,new HashSet<>(networkInterfaces));
         } catch (SocketException | IllegalArgumentException e) {
             LOG.error("DHCP server on port {} failed to decode option arguments {}", port, e);
             throw new IllegalStateException(e);
