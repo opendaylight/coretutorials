7691c7d12c468bb4edd27dfe92c962f65b083fc4
Martin Ciglan <martin.ciglan@pantheon.tech>
2017-06-29 11:26:18 +0200

    Binding v2 prototype - JMH Benchmark feature
    - able to run benchmark to compare binding v1 vs. v2
    code generation
    
    Change-Id: I3de4a9b8456642cedcdb639d7774ad5693b2e1ee
    Signed-off-by: Martin Ciglan <martin.ciglan@pantheon.tech>

---
+++ binding2-prototype/binding2-benchmark/src/main/java/org/opendaylight/coretutorials/MyBenchmark.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.coretutorials;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.List;
+import org.opendaylight.mdsal.binding.javav2.generator.api.BindingGenerator;
+import org.opendaylight.mdsal.binding.javav2.generator.impl.BindingGeneratorImpl;
+import org.opendaylight.mdsal.binding.javav2.model.api.Type;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
+import org.opendaylight.yangtools.yang.parser.spi.meta.ReactorException;
+import org.opendaylight.yangtools.yang.test.util.YangParserTestUtils;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+/**
+ * $ cd binding2-benchmark/
+ * $ mvn clean install
+ * Running the benchmarks. After the build is done, you will get the self-contained executable JAR,
+ *     which holds your benchmark, and all essential JMH infrastructure code:
+ * $ java -jar target/benchmarks.jar
+ */
+@State(Scope.Benchmark)
+public class MyBenchmark {
+
+    private SchemaContext context = null;
+
+    @Setup
+    public void setup() throws ReactorException, FileNotFoundException, URISyntaxException {
+        context = YangParserTestUtils.parseYangSources(getClass().getResource("/yangs/").getPath());
+    }
+
+    @Benchmark
+    public void testBindingV1() {
+        final org.opendaylight.mdsal.binding.generator.api.BindingGenerator bindingGenerator =
+                new org.opendaylight.mdsal.binding.generator.impl.BindingGeneratorImpl(true);
+        final List<org.opendaylight.mdsal.binding.model.api.Type> types = bindingGenerator.generateTypes(context, context.getModules());
+    }
+
+    @Benchmark
+    public void testBindingV2() throws ReactorException, FileNotFoundException, URISyntaxException {
+        final BindingGenerator bindingGenerator = new BindingGeneratorImpl(true);
+        final List<Type> types = bindingGenerator.generateTypes(context, context.getModules());
+    }
+}
