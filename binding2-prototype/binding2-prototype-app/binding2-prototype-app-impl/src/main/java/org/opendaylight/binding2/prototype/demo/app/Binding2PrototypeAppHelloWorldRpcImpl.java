d8ae6b99eb655944182bbecaabbebdec5ac19acc
Jakub Toth <jakub.toth@pantheon.tech>
2017-07-07 15:28:22 +0200

    Rpc registry
    Change-Id: I1214f57b5755402d0186db128bda3d39a5375f45
    Signed-off-by: Jakub Toth <jakub.toth@pantheon.tech>

---
+++ binding2-prototype/binding2-prototype-app/binding2-prototype-app-impl/src/main/java/org/opendaylight/binding2/prototype/demo/app/Binding2PrototypeAppHelloWorldRpcImpl.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.binding2.prototype.demo.app;
+
+import org.opendaylight.mdsal.binding.javav2.spec.base.RpcCallback;
+import org.opendaylight.mdsal.gen.javav2.urn.opendaylight.params.xml.ns.yang.binding2.prototype.app.rev170505.Binding2PrototypeAppHelloWorldRpc;
+import org.opendaylight.mdsal.gen.javav2.urn.opendaylight.params.xml.ns.yang.binding2.prototype.app.rev170505.data.hello_world.HelloWorldInput;
+import org.opendaylight.mdsal.gen.javav2.urn.opendaylight.params.xml.ns.yang.binding2.prototype.app.rev170505.data.hello_world.HelloWorldOutput;
+import org.opendaylight.mdsal.gen.javav2.urn.opendaylight.params.xml.ns.yang.binding2.prototype.app.rev170505.dto.hello_world.HelloWorldOutputBuilder;
+
+public class Binding2PrototypeAppHelloWorldRpcImpl implements Binding2PrototypeAppHelloWorldRpc {
+
+    @Override
+    public void invoke(final HelloWorldInput input, final RpcCallback<HelloWorldOutput> callback) {
+        final HelloWorldOutputBuilder helloWorldOutputBuilder = new HelloWorldOutputBuilder();
+        helloWorldOutputBuilder.setGreeting("Hello " + input.getName());
+
+        callback.onSuccess(helloWorldOutputBuilder.build());
+    }
+
+}
